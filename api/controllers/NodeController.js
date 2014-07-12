45eeec2f96758eaf0406fc530a263c3cfc367631
Akshay Dixit <akshaydixi@gmail.com>
2014-07-12 09:55:20 -0400

    added functions to NodeController

--- api/controllers/NodeController.js
+++ api/controllers/NodeController.js
@@ -10,7 +10,7 @@ var async = require("async");
 
 var FB = require("fb");
 
-var addPlaceHelper = function(data){
+var addPlaceHelper = function(data, placelocation){
                 for (var i = 0; i < data.businesses.length; i++) {
                         var place = data.businesses[i];
                         var query =
@@ -31,7 +31,8 @@ var addPlaceHelper = function(data){
                                                 yelpName: place['name'],
                                                 yelpReviewCount: place['review_count'],
                                                 yelpImageUrl: place['image_url'],
-                                                yelpPhoneNo: place['display_phone']
+                                                yelpPhoneNo: place['display_phone'],
+																								yelpLocation: placelocation
                                                 //yelpCategories: place['categories'],
                                                 //yelpLocation: '"'+place.location.coordinate.longitude+","+place.location.coordinate.latitude+'"'
                                                 });
@@ -89,18 +90,15 @@ module.exports = {
 	}, // end addFriends
 
 	addPlaces: function(req, res) {
-		var functionArray = [];
-		//var place_name = req.param('place');
-		place_name = "Mountain View"; // for debugging only
+		var place_name = req.param('place');
+		//place_name = "Mountain View"; // for debugging only
 		//scraping yelp
-		for(o= 0; o<1000; o+= 20){
+		for(o= 0; o<100; o+= 20){
 			// See http://www.yelp.com/developers/documentation/v2/search_api
 			yelp.search({term: "restaurant", location: place_name, offset: o}, 
 				function(error, data) {
-				console.log(data.businesses);
-				console.log(data.businesses.length);
 				if (data.businesses==undefined) console.log(data);
-				addPlaceHelper(data);	
+				addPlaceHelper(data, place_name);	
 			/*
 				for (var i = 0; i < data.businesses.length; i++) {
 					var place = data.businesses[i];
@@ -142,10 +140,65 @@ module.exports = {
 		//return places;
 	},
 
-suggestPlaces : function(req, res) {
+createSimilarityForPlace : function(req, res) {
 		// Return a list of suggested places by the recommendation algorithm
+		FB.setAccessToken(req.user.accessToken);
+		var alocation = req.param('location');
+		var query = " MATCH (n {yelpLocation: {location} ) return n; ";
+		var params = { location : alocation };
+		db.query(query, params, function(err, results) {
+			for ( var i = 0; i < results.length; i++) {
+				var place = results[i].n._data.data;
+				var categories = place['yelpCategories'];
+				var point = 0;
+				for ( var j = 0; j < categories.length; j++ ) {
+					var category = categories[j][0];
+					FB.api('/me/likes', {fields : ['name']}, function(res) {
+						for ( var k = 0; k < res['data'].length; k++) {
+							var item = res['data'][k]['name'];
+							if (item == category ) point++; 
+						}
+					});
+					
+				}	
+				var reviewCount = place['yelpReviewCount'];
+				var rating = (point) * ((reviewCount/1000) + 1);
+				var query = "START n = node(*), m = node(*) WHERE HAS(n.tmpnode) AND HAS(n.facebookId) AND HAS(m.yelpId) AND n.facebookId = {facebookId} AND m.yelpId = {yelpID} CREATE UNIQUE (n)-[r:RATED { rating : {Rating} } ]->(m) RETURN r";
+				var params = {
+					facebookId = req.user.facebookId,
+					yelpId = place['yelpId'],
+					Rating : rating
+				};
+				db.query(query, params, function(err, result) { 
+					if(err) { console.log(err); }
+				});
+			}
+		});
+var query = "MATCH (p {facebookId : {facebookID}, tmpnode:true})-[x:RATED]->(n)<-[y:RATED]-(p2 {tmpnode: true}) WITH SUM(x.rating*y.rating) AS xyDotProduct, SQRT(REDUCE(xDot = 0, a IN COLLECT(x.rating) | xDot + a^2)) AS xLength, SQRT(REDUCE(yDot = 0, b in COLLECT(y.rating) | yDot + b^2)) AS yLength, p,p2 CREATE UNIQUE (p)-[s:SIMILARITY]-(p2) SET s.similarity = (xyDotProduct / (xLength * yLength))";
+var params = {
+		facebookID : req.user.facebookId,
+	}
+db.query(query,params, function(err, res) { if(err) console.log(err); });
+res.send(200);
 },
 
+generateSuggestions : function(req, res) {
+	var query = "MATCH (b {tmpnode:true} )-[r:RATED]->(m), (b)-[s:SIMILARITY]-(a {tmpnode:true, facebookId: {facebookID} }) WHERE NOT((a)-[:RATED}->(m)) WITH m,s.similarity AS similariy, r.rating AS rating ORDER BY m.yelpName, similarity DESC return m, similarity";
+	var params = {
+		facebookID : req.user.facebookId;
+	};
+	var data = [];
+	db.query(query, params, function(err, results) { if(err) console.log(err);
+	for(var i = 0; i < results.length; i++) {
+		var t = results[i].m.data._data;
+		data.push(t);
+	}
+	res.send(data);
+	
+	
+ });
+
+},
 addSuggestions : function(req, res) {
 	// Add a suggestion by a user
 	console.log(req.params);
@@ -172,6 +225,12 @@ joinEvent : function(req, res) {
 	});
 	}
 	});
+},
+
+
+updateEvent: function(req , res) {
+	var eventId = req.param('eventId');
+	
 },
 
 createEvent : function(req, res) {
