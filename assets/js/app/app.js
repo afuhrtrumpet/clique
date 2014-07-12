8e9fc2c98c832352c8c28d87720cfb333b81d0f9
Prashish Rajbhandari <prashishh@gmail.com>
2014-07-11 20:38:31 -0700

    angular structure done

---
+++ assets/js/app/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var cliqueApp = angular.module('cliqueApp', ['ngRoute', 'ngResource', 'ui.bootstrap'])
+	.config(function($routeProvider, $locationProvider) {
+
+	$routeProvider
+		.when('/home', {
+			templateUrl: 'partials/home',
+			controller: 'homeController'
+		})
+		.otherwise({
+			redirectTo: '/home'
+		});
+
+	$locationProvider.html5Mode(true);
+})
+.run(function($rootScope) {
+
+});
--- config/routes.js
+++ config/routes.js
@@ -34,6 +34,9 @@ module.exports.routes = {
   // (This would also work if you had a file at: `/views/home.ejs`)
   '/': {
     view: 'home/index'
+  },
+  '/partials/home': {
+    view: 'partials/home'
   }
 
   /*
