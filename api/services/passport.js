3ac4b9b5e6415fceecb07a979aaf37130704ee59
afuhrtrumpet@gmail.com <afuhrtrumpet>
2014-08-11 11:49:03 -0700

    Added user profile picture support

--- api/services/passport.js
+++ api/services/passport.js
@@ -1,88 +1,89 @@
 var passport = require('passport'),
-  FacebookStrategy = require('passport-facebook').Strategy;
+		FacebookStrategy = require('passport-facebook').Strategy;
 var FB = require("fb");
 
 function findById(id, fn) {
-  User.findOne(id).done(function (err, user) {
-    if (err) {
-      return fn(null, null);
-    } else {
-      return fn(null, user);
-    }
-  });
+	User.findOne(id).done(function (err, user) {
+		if (err) {
+			return fn(null, null);
+		} else {
+			return fn(null, user);
+		}
+	});
 }
 
 function findByFacebookId(id, fn) {
-  User.findOne({
-    facebookId: id
-  }).done(function (err, user) {
-    if (err) {
-      return fn(null, null);
-    } else {
-      return fn(null, user);
-    }
-  });
+	User.findOne({
+		facebookId: id
+	}).done(function (err, user) {
+		if (err) {
+			return fn(null, null);
+		} else {
+			return fn(null, user);
+		}
+	});
 }
 
 passport.serializeUser(function (user, done) {
-  done(null, user.id);
+	done(null, user.id);
 });
 
 passport.deserializeUser(function (id, done) {
-  findById(id, function (err, user) {
-    done(err, user);
-  });
+	findById(id, function (err, user) {
+		done(err, user);
+	});
 });
 
 passport.use(new FacebookStrategy({
-    clientID: "574894572634219",
-    clientSecret: "5ed6ad8e9c13a2495f25079c7e3f369a",
-	  callbackURL: "/user/facebook/callback",
-	  enableProof: false
-  }, function (accessToken, refreshToken, profile, done) {
-		//To make a Facebook API call:
-		/*FB.api('me/picture', {
-			access_token: accessToken
+	clientID: "574894572634219",
+	clientSecret: "5ed6ad8e9c13a2495f25079c7e3f369a",
+	callbackURL: "/user/facebook/callback",
+	enableProof: false
+}, function (accessToken, refreshToken, profile, done) {
+	//To make a Facebook API call:
+	/*FB.api('me/picture', {
+		access_token: accessToken
 		}, function(result) {
-			console.log(JSON.stringify(result));
+		console.log(JSON.stringify(result));
 		});*/
 
-		console.log(accessToken);
+	//Get user's Facebook profile picture
 
-    findByFacebookId(profile.id, function (err, user) {
+	console.log(accessToken);
 
-			console.log(JSON.stringify(profile));
+	findByFacebookId(profile.id, function (err, user) {
 
-      // Create a new User if it doesn't exist yet
-      if (!user) {
-        User.create({
+		// Create a new User if it doesn't exist yet
+		if (!user) {
 
-          facebookId: profile.id,
-					name: profile.displayName
+			User.create({
 
-          // You can also add any other data you are getting back from Facebook here 
-          // as long as it is in your model
+				facebookId: profile.id,
+				name: profile.displayName,
 
-        }).done(function (err, user) {
-          if (user) {
-						user.accessToken = accessToken;
-            return done(null, user, {
-              message: 'Logged In Successfully'
-            });
-          } else {
-            return done(err, null, {
-              message: 'There was an error logging you in with Facebook'
-            });
-          }
-        });
+				// You can also add any other data you are getting back from Facebook here 
+				// as long as it is in your model
 
-      // If there is already a user, return it
-      } else {
-				user.accessToken = accessToken;
-				return done(null, user, {
-					message: 'Logged In Successfully'
-				});
-      }
-    });
-  }
+			}).done(function (err, user) {
+				if (user) {
+					user.accessToken = accessToken;
+					return done(null, user, {
+						message: 'Logged In Successfully'
+					});
+				} else {
+					return done(err, null, {
+						message: 'There was an error logging you in with Facebook'
+					});
+				}
+			});
+
+			// If there is already a user, return it
+		} else {
+			user.accessToken = accessToken;
+			return done(null, user, {
+				message: 'Logged In Successfully'
+			});
+		}
+	});
+}
 ));
